148960de78ac96e3ecfc2c1dce7e90b679428336
rern <rernrern@gmail.com>
2021-03-02 10:19:22 +0700

    Update repoupdate.sh

--- repoupdate.sh
+++ repoupdate.sh
@@ -4,7 +4,7 @@ rm $0
 
 updateRepo() {
 	# recreate database
-	cd /home/x/GitHub/rern.github.io/$1
+	cd /run/media/x/BIG/RPi/Git/rern.github.io/$1
 	rm -f +R*
 	repo-add -R +R.db.tar.xz *.pkg.tar.xz
 	
