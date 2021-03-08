1a7ce6da04b93f915a3108fb7e17d31249a2f7d8
rern <rernrern@gmail.com>
2021-03-08 14:40:01 +0700

    Update distcc.sh

--- distcc.sh
+++ distcc.sh
@@ -22,8 +22,47 @@ case $arch in
 	3 ) arch=armv8;;
 esac
 
-systemctl stop distccd-arm*
+if [[ ! -e /usr/bin/distccd ]]; then
+	pacman -Sy distcc
+	clientip=$( dialog "${optbox[@]}" --output-fd 1 --inputbox "
+ Client IP:
+
+" 0 0 192.168.1.9 )
+	if [[ -e /boot/kernel8.img ]]; then
+		port=3636
+	elif [[ -e /boot/kernel7.img ]]; then
+		port=3635
+	else
+		port=3634
+	fi
+	cores=$( lscpu | awk '/^Core/ {print $NF}' )
+	if (( $cores == 4 )); then
+		jobs=12
+		masterip=$( ifconfig | awk '/inet.*broadcast 192/ {print $2}' )
+		hosts="$clientip:$port/$jobs $masterip:$port/$cores"
+		dir=/etc/systemd/system/distccd.service.d
+		mkdir -p $dir
+		cat << 'EOF' > $dir/override.conf
+[Service]
+ExecStart=
+ExecStart=/usr/bin/taskset -c 3 /usr/bin/distccd --no-detach --daemon $DISTCC_ARGS
+EOF
+		systemctl daemon-reload
+	else
+		jobs=8
+		hosts="$clientip:$port/$jobs"
+	fi
+	sed -i -e 's/^#*\(MAKEFLAGS="-j\).*/\1'$jobs'"/
+	' -e 's/!distcc/distcc/
+	' -e "s|^#*\(DISTCC_HOSTS=\"\).*|\1$hosts\"|
+	" /etc/makepkg.conf
+else
+	systemctl stop distccd-arm*
+fi
+
 systemctl start distccd-$arch
 status=$( systemctl status distccd-$arch | sed 's/active (running)/\\e[32m&\\e[0m/' )
 clear -x
-echo -e "$status"
+echo -e "\
+\e[32mdistccd-$arch\e[0m
+$status"
