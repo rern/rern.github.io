2f3573d7f5d23300a7daacc242ddc0a2eb77fcac
rern <rernrern@gmail.com>
2021-03-11 16:57:53 +0700

    Update and rename updateget.sh to pkgupdatelist.sh

--- pkgupdatelist.sh
+++ pkgupdatelist.sh
@@ -2,7 +2,9 @@
 
 for pkg in bluez-alsa-git fakepkg hfsprogs kid3-cli matchbox-window-manager mpdscribble snapcast upmpdcli; do
 	version=$( curl -L "https://aur.archlinux.org/packages/?O=0&K=$pkg" \
-				| grep -A1 '<td>.*upmpdcli' \
+				| grep -A1 "<td>.*$pkg" \
 				| tail -1 \
 				| sed 's/.*td>\(.*\)<.*/\1/' )
+	existing=$( pacman -Q $pkg | cut -d' ' -f2 )
+	[[ $version != $existing ]] && echo $pkg $existing --- $version
 done
