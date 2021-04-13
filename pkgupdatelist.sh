34c98bd1ee4e9ed23fe1d0fec9ae40d523ec9f3f
rern <rernrern@gmail.com>
2021-04-13 11:19:07 +0700

    Update pkgupdatelist.sh

--- pkgupdatelist.sh
+++ pkgupdatelist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for pkg in bluez-alsa-git fakepkg hfsprogs kid3-cli matchbox-window-manager mpdscribble snapcast upmpdcli; do
+for pkg in bluez-alsa-git fakepkg hfsprogs matchbox-window-manager mpdscribble snapcast upmpdcli; do
 	version=$( curl -L "https://aur.archlinux.org/packages/?O=0&K=$pkg" \
 				| grep -A1 "<td>.*$pkg" \
 				| tail -1 \
