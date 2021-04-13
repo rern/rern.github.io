dab153c5d5ea0a451f583cb69ff79244e7348309
rern <rernrern@gmail.com>
2021-04-13 11:46:54 +0700

    Update pkgupdatelist.sh

--- pkgupdatelist.sh
+++ pkgupdatelist.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for pkg in bluez-alsa-git fakepkg hfsprogs matchbox-window-manager mpdscribble snapcast upmpdcli; do
+for pkg in bluez-alsa fakepkg hfsprogs matchbox-window-manager mpdscribble snapcast upmpdcli; do
 	version=$( curl -L https://aur.archlinux.org/packages/$pkg \
-				| grep -A1 "<td>.*$pkg" \
-				| tail -1 \
-				| sed 's/.*td>\(.*\)<.*/\1/' )
-	existing=$( pacman -Q $pkg | cut -d' ' -f2 )
+				| grep 'Package Details:' \
+				| sed 's/.*h2>\(.*\)<.*/\1/' \
+				| cut -d' ' -f3 )
+	existing=$( pacman -Q ${pkg/-git} | cut -d' ' -f2 )
 	[[ $version != $existing ]] && echo $pkg $existing --- $version
 done
