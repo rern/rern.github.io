b9e98e4feebc145635e65ad6b5a3d6cc016ca0f5
rern <rernrern@gmail.com>
2021-04-29 08:49:51 +0700

    Create distcc-install-client.sh

---
+++ distcc-install-client.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+optbox=( --colors --no-shadow --no-collapse )
+
+dialog "${optbox[@]}" --infobox "
+                 Install \Z1Distcc\Z0 Client
+" 9 58
+sleep 1
+
+pacman -Sy --noconfirm distcc
+clientip=$( dialog "${optbox[@]}" --output-fd 1 --inputbox "
+Client IP:
+" 0 0 192.168.1.9 )
+
+ for arch in armv6h armv7h armv8; do
+ 	wget https://github.com/rern/distcc-alarm/releases/download/20200823/distccd-alarm-$arch-10.2.0.20200823-3-x86_64.pkg.tar.zst
+	pacman -U distccd-alarm-$arch
+ done
+ rm distccd-alarm-*
+ 
