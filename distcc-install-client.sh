1846b34fa37045b000de580718e2852beb6e45dd
rern <rernrern@gmail.com>
2021-06-03 12:56:25 +0700

    Update distcc-install-client.sh

--- distcc-install-client.sh
+++ distcc-install-client.sh
@@ -12,10 +12,10 @@ clientip=$( dialog "${optbox[@]}" --output-fd 1 --inputbox "
 Client IP:
 " 0 0 192.168.1.9 )
 
-# AUR:
-# https://aur.archlinux.org/packages/distccd-alarm-armv6h
-# https://aur.archlinux.org/packages/distccd-alarm-armv7h
-# https://aur.archlinux.org/packages/distccd-alarm-armv8
+# build toolchains:
+# curl -L https://aur.archlinux.org/cgit/aur.git/snapshot/distccd-alarm.tar.gz | bsdtar xf -
+# cd distccd-alarm
+# makepkg
 for arch in armv6h armv7h armv8; do
 	wget https://github.com/rern/distcc-alarm/releases/download/20200823/distccd-alarm-$arch-10.2.0.20200823-3-x86_64.pkg.tar.zst
 	pacman -U distccd-alarm-$arch
