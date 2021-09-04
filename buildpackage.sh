c68eda63ba98480eba23f985236468da3bb08783
rern <rernrern@gmail.com>
2021-09-04 10:46:16 +0700

    Update buildpackage.sh

--- buildpackage.sh
+++ buildpackage.sh
@@ -17,17 +17,17 @@ declare -A packages=(
 	[cava]=fftw
 	[fakepkg]=gzip
 	[hfsprogs]=libbsd
-	[gconf]='polkit dbus-glib intltool gtk-doc gobject-introspection gnome-common'
-	[libmatchbox]='pango libpng libjpeg xsettings-client libxext'
-	[matchbox-window-manager]='libmatchbox startup-notification libpng libsm libxcursor'
+	[matchbox-window-manager]='dbus-glib gnome-common gobject-introspection gtk-doc intltool \
+								libjpeg libmatchbox libpng libsm libxcursor libxext \
+								pango polkit startup-notification xsettings-client'
 	[mpdscribble]='boost libmpdclient libsoup meson ninja'
 	[nginx-mainline-pushstream]='geoip mailcap'
 	[p7zip-gui]='p7zip yasm wxgtk2'
 	[python-raspberry-gpio]=python-distribute
 	[snapcast]='boost cmake git'
-	[libnpupnp]='expat libmicrohttpd'
-	[libupnpp]='libnpupnp expat'
-	[upmpdcli]='aspell-en id3lib jsoncpp libmicrohttpd libmpdclient libupnpp python python-requests python-setuptools python-bottle python-mutagen python-waitress recoll sqlite'
+	[upmpdcli]='aspell-en expat id3lib jsoncpp libmicrohttpd libmpdclient libnpupnp libupnpp \
+				python python-requests python-setuptools python-bottle python-mutagen python-waitress \
+				recoll sqlite'
 )
 pkgs=( $( echo "${!packages[@]}" | tr ' ' '\n' | sort ) )
 pkgsL=${#pkgs[@]}
@@ -43,17 +43,35 @@ pkg=$( dialog "${optbox[@]}" --output-fd 1 --menu "
 pkgname=${pkgs[$pkg]}
 depends=${packages[$pkgname]}
 
+clear
+echo -e "\e[46m  \e[0m Install depends ...\n"
+
+[[ ! -e /usr/bin/fakeroot ]] && depends="$base-devel depends"
+
 pacman -S --noconfirm --needed $depends
 
 currentdir=$PWD
 
-cd /home/alarm
-curl -L https://aur.archlinux.org/cgit/aur.git/snapshot/$pkgname.tar.gz | bsdtar xf -
-chown -R alarm:alarm $pkgname
-cd $pkgname
-sudo -u alarm makepkg -A
+buildPackage() {
+	[[ $1 != -i ]] && pkgname=$1 || pkgname=$2
+	cd /home/alarm
+	curl -L https://aur.archlinux.org/cgit/aur.git/snapshot/$pkgname.tar.gz | bsdtar xf -
+	chown -R alarm:alarm $pkgname
+	cd $pkgname
+	sudo -u alarm makepkg -fA
+	mv -f $pkgname*.xz "$currentdir"
+	cd "$currentdir"
+	[[ $1 == -i ]] && pacman -U --noconfirm $pkgname*
+}
 
-mv -f $pkgname* "$currentdir"
-cd "$currentdir"
-echo Package:
-ls $pkgname*
+if [[ $pkgname == matchbox-window-manager ]]; then
+	buildPackage -i gconf
+	buildPackage -i libmatchbox
+elif [[ $pkgname == upmpdcli ]]; then
+	buildPackage -i libnpupnp
+	buildPackage -i libupnpp
+fi
+
+buildPackage $pkgname
+
+echo -e "\n\e[46m  \e[0m Package: $( ls $pkgname*.xz )\n"
