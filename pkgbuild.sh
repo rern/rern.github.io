4b8f8e6c2badd39678bec5f97fc1fe06b05c7441
rern <rernrern@gmail.com>
2021-12-23 10:32:01 +0700

    Update pkgbuild.sh

--- pkgbuild.sh
+++ pkgbuild.sh
@@ -60,7 +60,7 @@ pkg=$( dialog "${optbox[@]}" --output-fd 1 --menu "
 
 [[ ! -e /usr/bin/distccd ]] && bash <( curl -L https://github.com/rern/rern.github.io/raw/master/distcc-install-master.sh )
 
-[[ ! -e /usr/bin/fakeroot ]] && pkgdepends='base-devel '
+pkgdepends='base-devel '
 pkgname=${pkgs[$pkg]}
 pkgdepends+=${packages[$pkgname]}
 
