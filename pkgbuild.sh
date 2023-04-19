df823b1991da12fd53666764c7aa59e34d330687
rern <rernrern@gmail.com>
2023-04-19 17:12:28 +0700

    Update pkgbuild.sh

--- pkgbuild.sh
+++ pkgbuild.sh
@@ -50,6 +50,7 @@ declare -A packages=(
 	[matchbox-window-manager]='dbus-glib gnome-common gobject-introspection gtk-doc intltool
 								libjpeg libmatchbox libpng libsm libxcursor libxext
 								pango polkit startup-notification xsettings-client'
+	[mediamtx]=go
 	[mpd]='audiofile avahi boost chromaprint faad2 ffmpeg flac fluidsynth fmt jack
 			lame libao libcdio libcdio-paranoia libgme libid3tag libmad libmikmod libmms libmodplug libmpcdec libnfs libogg
 			libopenmpt libpipewire libpulse libsamplerate libshout libsidplayfp libsndfile libsoxr libupnp liburing libvorbis
@@ -61,7 +62,6 @@ declare -A packages=(
 	[python-rplcd]=
 	[python-smbus2]=
 #	[raspberrypi-firmware]=
-	[rtsp-simple-server]=go
 	[snapcast]='boost cmake'
 	[upmpdcli]='aspell-en expat id3lib jsoncpp libmicrohttpd libmpdclient
 				python python-requests python-setuptools python-bottle python-mutagen python-waitress
