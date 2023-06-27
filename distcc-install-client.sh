83eb61428992428a01397f7412fa5a409eb154c5
rern <rernrern@gmail.com>
2023-06-27 16:15:14 +0700

    Update distcc-install-client.sh

--- distcc-install-client.sh
+++ distcc-install-client.sh
@@ -14,10 +14,14 @@ pacman -Sy --noconfirm distcc
 # for ssh start distccd from master
 sed -i -e 's/#\(PermitRootLogin \).*/\1yes/' /etc/ssh/sshd_config
 systemctl enable sshd
+[[ ! -e /usr/bin/distccd-armv8 ]] && pkg=distccd-armv8
+[[ ! -e /usr/bin/distccd-armv6h ]] && pkg+=' distccd-armv6h'
+[[ ! $pkg ]] && exit
 
 dialog "${optbox[@]}" --infobox "
 
 
           Please build and install toolchains
+		  $pkg
 		  
 " 9 58
