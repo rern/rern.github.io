62154deab070da8dac56124f56ad66f335393ad5
rern <rernrern@gmail.com>
2023-11-29 20:39:04 +0700

    Update repoupdate.sh

--- repoupdate.sh
+++ repoupdate.sh
@@ -2,41 +2,6 @@
 
 dircurrent=$PWD
 
-updateRepo() {
-	echo -e "\n\n\e[44m  \e[0m Update repository $1 ...\n"
-	cd $dirrepo/$1
-	repo-add -n -R +R.db.tar.xz *.pkg.tar.xz
-	rm -f *.xz.old
-	# index.html
-	html='<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>+R Packages</title>
-	<style>
-		table { font-family: monospace; white-space: pre; border: none }
-		td:last-child { padding-left: 10px; text-align: right }
-	</style>
-</head>
-<body>
-<table>
-	<tr><td><a href="/">../</a></td><td></td></tr>'
-	pkg=( $( ls *.pkg.tar.xz ) )
-	readarray -t sizedate <<<$( ls -lh --time-style='+%Y%m%d %H:%M' *.pkg.tar.xz | tr -s ' ' | cut -d' ' -f5-7 )
-	pkgL=${#pkg[@]}
-	for (( i=1; i < $pkgL; i++ )); do
-		pkg=${pkg[$i]}
-		html+='
-	<tr><td><a href="'$1'/'$pkg'">'$pkg'</a></td><td>'${sizedate[$i]}'</td></tr>'
-	done
-	html+='
-<table>
-</body>
-</html>'
-
-	echo -e "$html" > ../$1.html
-}
-
 dirrepo=$PWD/repo
 mkdir -p $dirrepo
 if [[ ! $( ls /boot/kernel* 2> /dev/null ) ]]; then # not RPi
@@ -66,14 +31,57 @@ fi
 arch=$( dialog --colors --output-fd 1 --checklist '\n\Z1Arch:\Z0' 9 30 0 \
 	1 aarch64 on \
 	2 armv7h on \
-	3 armv6h on )
+	3 armv6h on \
+	4 \\Z1Rebuild\\Z0 off )
+if [[ $arch == *4 ]]; then
+	action=Rebuild
+else
+	action=Update
+	new=-n # newer only (deleted packages still exist in db)
+fi
+
 for i in $arch; do
 	case $i in
-		1 ) updateRepo aarch64;;
-		2 ) updateRepo armv7h;;
-		3 ) updateRepo armv6h;;
+		1 ) arch=aarch64;;
+		2 ) arch=armv7h;;
+		3 ) arch=armv6h;;
+		4 ) break;;
 	esac
+	
+	echo -e "\n\n\e[44m  \e[0m $action repository $arch ...\n"
+	cd $dirrepo/$arch
+	repo-add $new -R +R.db.tar.xz *.pkg.tar.xz
+	rm -f *.xz.old
+	# index.html
+	html='<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>+R Packages</title>
+	<style>
+		table { font-family: monospace; white-space: pre; border: none }
+		td:last-child { padding-left: 10px; text-align: right }
+	</style>
+</head>
+<body>
+<table>
+	<tr><td><a href="/">../</a></td><td></td></tr>'
+	pkg=( $( ls *.pkg.tar.xz ) )
+	readarray -t sizedate <<<$( ls -lh --time-style='+%Y%m%d %H:%M' *.pkg.tar.xz | tr -s ' ' | cut -d' ' -f5-7 )
+	pkgL=${#pkg[@]}
+	for (( i=1; i < $pkgL; i++ )); do
+		pkg=${pkg[$i]}
+		html+='
+	<tr><td><a href="'$arch'/'$pkg'">'$pkg'</a></td><td>'${sizedate[$i]}'</td></tr>'
+	done
+	html+='
+<table>
+</body>
+</html>'
+
+	echo -e "$html" > ../$arch.html
 done
+
 cd "$dircurrent"
 if [[ -L repo ]]; then
 	rm -r repo
