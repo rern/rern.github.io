d55d2e5796da1b2cd4fa315e45ea1128a6913114
rern <rernrern@gmail.com>
2024-03-10 16:42:25 +0700

    Update firmware.sh

--- firmware.sh
+++ firmware.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-url=http://sg.mirror.archlinuxarm.org/armv7h
-listalarm=$( curl -L $url/alarm )
-listcore=$( curl -L $url/core )
-packages='firmware-raspberrypi linux-firmware linux-firmware-whence raspberrypi-bootloader'
-for pkg in $packages; do
-	rm -f *.xz
-	if [[ ${pkg:0:1} == l ]]; then
-		href=$url/core/$( grep -m1 $pkg <<< $listcore | cut -d'"' -f2 )
-	else
-		href=$url/alarm/$( grep -m1 $pkg <<< $listalarm | cut -d'"' -f2 )
-	fi
-	wget $href
-done
-pacman -U --needed *.xz
+fileconf=/etc/pacman-armv6h.conf
+if [[ ! -e $fileconf ]]; then
+	repo='
+[alarm]
+SigLevel = PackageRequired
+Server = https://sg.mirror.archlinuxarm.org/armv7h/$repo
+'
+	repo+=${repo/alarm/core}
+	echo "$repo" > $fileconf
+fi
+pacman -Syy --needed --noconfirm --config $fileconf firmware-raspberrypi linux-firmware linux-firmware-whence raspberrypi-bootloader
