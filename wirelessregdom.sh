8dfe3a0ba5b6ef94746ebacf6d1505e247781ab0
rern <rernrern@gmail.com>
2024-05-17 09:13:56 +0700

    Update wirelessregdom.sh

--- wirelessregdom.sh
+++ wirelessregdom.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-codes=$( curl -skL https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/plain/db.txt \
-				| grep ^country \
-				| cut -d' ' -f2 \
-				| tr -d : \
-				| xargs \
-				| tr ' ' '|')
+url=https://kernel.org/pub/software/network/wireless-regdb
+file=$( curl -skL $url \
+			| tail -3 \
+			| head -1 \
+			| cut -d'"' -f2 )
+curl -skL $url/$file | bsdtar xf - wireless-regdb*/db.txt
+codes=$( grep ^country wireless-regdb*/db.txt \
+			| cut -d' ' -f2 \
+			| tr -d : \
+			| xargs \
+			| tr ' ' '|' )
 iso3166=$( sed -E -n '/alpha_2_code=|\s+name=/ {s/^.*name=/:/; s/^.*code=/, /; s/ .>$//; p}' /usr/share/xml/iso-codes/iso_3166.xml )
 list=$( echo '{ "00": "00"'$iso3166' }' \
 			| jq \
