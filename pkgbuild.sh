b7ed797daff32cb6ea3ab44a10d3194efb08c245
rern <rernrern@gmail.com>
2024-08-28 09:22:23 +0700

    Update pkgbuild.sh

--- pkgbuild.sh
+++ pkgbuild.sh
@@ -78,7 +78,7 @@ pkgname=$( dialog "${optbox[@]}" --output-fd 1 --no-items --menu "
 
 [[ $? != 0 ]] && exit
 
-[[ $pkgname == snapcast ]] &&  && echo 'snapcast requires swap partition.' && exit
+[[ $pkgname == snapcast ]] && echo 'snapcast requires swap partition.' && exit
 
 urlrern=https://github.com/rern/rern.github.io/raw/main
 if [[ $pkgname == wirelessregdom-codes ]]; then
