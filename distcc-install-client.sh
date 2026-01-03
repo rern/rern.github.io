986ef575942d1d287012fbe003d59b3766dee263
rern <rernrern@gmail.com>
2026-01-03 13:33:30 +0700

    Update distcc-install-client.sh

--- distcc-install-client.sh
+++ distcc-install-client.sh
@@ -14,15 +14,12 @@ pacman -Sy --noconfirm distcc
 # for ssh start distccd from master
 sed -i -e 's/#\(PermitRootLogin \).*/\1yes/' /etc/ssh/sshd_config
 systemctl enable sshd
-[[ ! -e /usr/bin/distccd-armv8 ]] && pkg8='https://aur.archlinux.org/packages/distccd-alarm-armv8'
-[[ ! -e /usr/bin/distccd-armv7h ]] && pkg7='https://aur.archlinux.org/packages/distccd-alarm-armv7h'
-[[ ! $pkg && ! $pkg7 ]] && exit
+[[ -e /usr/bin/distccd-armv8 ]] && [[ -e /usr/bin/distccd-armv7h ]] && exit
 
 dialog "${optbox[@]}" --infobox "
 
 
           Please build and install toolchains:
-     $pkg8
-	 $pkg7
+ https://aur.archlinux.org/packages/distccd-alarm-armv8
 		  
 " 9 58
