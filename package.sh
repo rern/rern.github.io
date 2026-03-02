0e92c33417b234f1ed2a605074cba384b99fb479
rern <rernrern@gmail.com>
2026-03-02 20:42:22 +0700

    u

--- package.sh
+++ package.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 
 . <( curl -sL https://github.com/rern/rOS/raw/main/common.sh )
+
+! grep -q ^Model.*Rasp /proc/cpuinfo && errorExit This is not a Raspberry Pi
+#----------------------------------------------------------------------------
 #........................
 dialogSplash Package Utilities
-list_menu="\
-Build Package
-Update Repo
-AUR Setup
+list="\
+Build Package^pkgbuild
+Update Repo^repoupdate
+AUR Setup^aursetup
 Create guide.tar.xz
 Create radioparadise.tar.xz
-Create regdomcodes.json"
+Create regdomcodes.json^wirelessregdom"
 #........................
-menu=$( dialogMenu Package "$list_menu" )
+task=$( dialogMenu Package "$( sed 's/\^.*//' <<< $list )" )
+name=$( sed -n "$task {s/.*^//; p}" <<< $list )
 #........................
-dialogSplash $( sed -n "$menu p" <<< $list_menu )
-case $menu in
-	1 ) 
-		arch=$( pacman -Qi bash | awk '/^Arch/ {print $NF}' )
-		[[ ! $arch =~ .*(aarch|armv).* ]] && errorExit This is not a Raspberry Pi
-		#----------------------------------------------------------------------------
-		file=pkgbuild
-		;;
-	2 ) file=repoupdate;;
-	3 ) file=aursetup;;
-	4 )	bsdtar cjvf guide.tar.xz -C /srv/http/assets/img/guide .; exit;;
-	5 )	bsdtar cjvf radioparadise.tar.xz -C /srv/http/data/webradio .; exit;;
-	6 ) file=wirelessregdom;;
-esac
-. <( curl -sL https://github.com/rern/rern.github.io/raw/main/$file.sh )
+dialogSplash $name
+if [[ $name ]]; then
+	. <( curl -sL https://github.com/rern/rern.github.io/raw/main/$name.sh )
+elif grep -q guide <<< $name
+	bsdtar cjvf guide.tar.xz -C /srv/http/assets/img/guide .
+elif grep -q radio <<< $name
+	bsdtar cjvf radioparadise.tar.xz -C /srv/http/data/webradio .
+fi
