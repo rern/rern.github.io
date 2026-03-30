66e36cb643ac6994bc34a5e8a62e21eddb7a2bd0
rern <rernrern@gmail.com>
2026-03-30 14:34:15 +0700

    u

--- package.sh
+++ package.sh
@@ -10,19 +10,12 @@ list="\
 Package Build        : package_build
 Repo Update          : repo_update
 AUR Setup            : aur_setup
-guide.tar.xz         :
-radioparadise.tar.xz :
 regdomcodes.json     : wireless_regdom"
 #........................
 task=$( dialog.menu Package "$( sed 's/ *:.*//' <<< $list )" )
 name=$( sed -n "$task {s/ *:.*//; p}" <<< $list )
-script=$( sed -n "$task {s/.*: *//; p}" <<< $list )
 #........................
 dialog.splash $name
 clear -x
-if [[ $script ]]; then
-	. <( curl -sL $https_io/$script.sh )
-else
-	grep -q guide <<< $name && dir=assets/img/guide || dir=data/webradio
-	bsdtar cjvf $name -C /srv/http/$dir .
-fi
+script=$( sed -n "$task {s/.*: *//; p}" <<< $list )
+. <( curl -sL $https_io/$script.sh )
--- wireless_regdom.sh
+++ wireless_regdom.sh
@@ -3,31 +3,16 @@
 #........................
 banner Wireless Regulatory Domain Codes
 bar Get wireless-regdb ...
-url=https://kernel.org/pub/software/network/wireless-regdb
-file=$( curl -skL $url \
-			| tail -3 \
-			| head -1 \
-			| cut -d'"' -f2 )
-curl -skL $url/$file | bsdtar xf - wireless-regdb*/db.txt
-codes=$( grep ^country wireless-regdb*/db.txt \
-			| cut -d' ' -f2 \
-			| tr -d : \
-			| xargs \
-			| tr ' ' '|' )
-iso3166=$( sed -E -n '/alpha_2_code=|\s+name=/ {
-				s/^.*name=/:/
-				s/^.*code=/, /
-				s/ .>$//
-				p
-			}' /usr/share/xml/iso-codes/iso_3166.xml )
-list=$( echo '{ "00 (worldwide)": "00"'$iso3166' }' \
-			| jq \
-			| grep -E "$codes" \
-			| sed -E 's/\s*"(.*)": "(.*)",*/"\2": "\1",/' \
-			| sed '/, / {s/, / (/; s/":/)":/}' )
+url=$( curl -sI https://kernel.org/pub/software/network/wireless-regdb | awk '/^location/ {sub(/\r/, ""); print $NF}' ) # \r must be removed
+file_db=$( curl -sL $url | awk '/href="wireless-regdb/ {line=$0} END {split(line, a, "\""); print a[2]}' )
+curl -sL $url/$file_db | bsdtar xf - --strip-components=1 wireless*/db.txt
+codes=$( awk '/^country/ {printf "|" substr($2, 1, 2)}' db.txt )
+country_code=$( jq '.["3166-1"] | sort_by(.name) | map({(.name): .alpha_2}) | add' /usr/share/iso-codes/json/iso_3166-1.json \
+					| grep -E "${codes:4}" )
+
 regdom=regdomcodes.json
 file_regdom=/srv/http/assets/data/$regdom
-jq -S <<< "{ ${list:0:-1} }" > $regdom
+jq <<< '{ "00 (worldwide)": "00", '${country_code:0:-1}'}' > $regdom
 diff=$( diff $regdom $file_regdom )
 if [[ $diff ]]; then
 	mv -f $regdom $file_regdom
