3b6c09ce1a49a0184796575c0d237096b6957f94
rern <rernrern@gmail.com>
2026-08-07 10:50:26 +0700

    u

--- repo_update.sh
+++ repo_update.sh
@@ -4,22 +4,17 @@
 
 updateRepo() {
 	[[ ! $newer_only ]] && rm -f +R*
-	repo-add $newer_only -R $1.db.tar.xz *.pkg.tar.xz *.pkg.tar.zst
+	repo-add $newer_only -R +R.db.tar.xz *.pkg.tar.xz
 	rm -f *.xz.old
 }
 
-if [[ $( uname -m ) == x86_64 ]]; then
-	manjaro=1
-else
-	bar Mount REPO ...
-	mkdir -p REPO
-	mount -t cifs //192.168.1.9/rern.github.io REPO -o username=guest,password=
-	echo 000
-	[[ $? != 0 ]] && rmdir REPO && dialog.error_exit "Mount '\Z1REPO\Zn' failed."
+bar Mount REPO ...
+mkdir -p REPO
+mount -t cifs //192.168.1.9/rern.github.io REPO -o username=guest,password=
+[[ $? != 0 ]] && rmdir REPO && dialog.error_exit "Mount '\Z1REPO\Zn' failed."
 #----------------------------------------------------------------------------
-	[[ ! $( ls REPO ) ]] && rmdir REPO && dialog.error_exit Repo empty: REPO
+[[ ! $( ls REPO ) ]] && rmdir REPO && dialog.error_exit Repo empty: REPO
 #----------------------------------------------------------------------------
-fi
 #........................
 selected=$( dialog $opt_check '
 \Z1Repository:\Z0
@@ -40,31 +35,16 @@ fi
 #........................
 banner $action Repository
 
+dir_base=$PWD
 shopt -s nullglob # suppress error if no *.zst
 for arch in $selected; do
-	if [[ $manjaro ]]; then
-		cd /home/x/GitHub/rern.github.io/$arch
-	else
-		dir_base=$PWD
-		cd $dir_base/REPO/$arch
-	fi
+	cd REPO/$arch
 	bar $arch
-	if [[ $arch == armv6h ]]; then
-		for dir in +R alarm core extra; do
-			bar $dir
-			cd $dir
-			updateRepo $dir
-			cd ..
-		done
-	else
-		updateRepo +R
-	fi
+	updateRepo
 done
 
-if [[ ! $manjaro ]]; then
-	cd $dir_base
-	umount -ql REPO
-	rmdir REPO
-fi
+cd $dir_base
+umount -ql REPO
+rmdir REPO
 
 bar Done
